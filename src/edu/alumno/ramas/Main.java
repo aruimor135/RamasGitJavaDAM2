09a0ebbb2fe05e3a4ea1d97db91ec70f5a4be44c
aruimor135 <aruimor135@g.educaand.es>
2025-11-14 13:58:06 +0100

    Mensaje alternativo en main

--- src/edu/alumno/ramas/Main.java
+++ src/edu/alumno/ramas/Main.java
@@ -21,6 +21,6 @@ public class Main {
 		int suma = bolas.stream().mapToInt(Integer::intValue).sum();
 		System.out.println("Suma de principales: " + suma);
 
-		System.out.println("¡Suerte en el sorteo, DAM!");
+		System.out.println("¡Buena suerte en la lotería!");
 	}
 }
